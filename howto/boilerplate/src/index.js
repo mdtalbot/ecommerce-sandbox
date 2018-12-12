4b65e7234fe9979b3b9333b2ffe8dcd2047f1829
Matthew Talbot <mdtalbot@gmail.com>
2018-12-12 18:23:56 -0500

    sandboxin

--- howto/boilerplate/src/index.js
+++ howto/boilerplate/src/index.js
@@ -1,3 +1,29 @@
 import './main.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import reducers from 'reducers'
+import { browserHistory, Router, Route } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { Provider } from 'react-redux'
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'
 
-document.write('Hello React/Redux!')
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunk)
+))
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route component={Layout}>
+        <Route path='/' component={Phones} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
